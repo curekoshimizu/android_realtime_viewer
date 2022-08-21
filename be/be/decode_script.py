80f9bdeb18b41dd395193f32bee5793c12403cde
Shugo Manabe <shugo.manabe@hacobu.jp>
2022-08-21 18:05:40 +0900

    implement sleep

--- be/be/decode_script.py
+++ be/be/decode_script.py
@@ -1,11 +1,15 @@
 import pathlib
 
+from tokenize import tokenize, ENCODING, NAME, OP, NUMBER, ENDMARKER, NEWLINE, TokenInfo
 from .adb_helper import AdbHelper
 
 shortcuts_dir = pathlib.Path(__file__).parents[1] / "assets" / "shortcuts"
 
 
 class DecodeScript:
+    def __init__(self, adb: AdbHelper) -> None:
+        self._adb = adb
+
     def scripts(self) -> list[str]:
         assert shortcuts_dir.exists(), shortcuts_dir
         assert shortcuts_dir.is_dir()
@@ -13,8 +17,50 @@ class DecodeScript:
         for p in shortcuts_dir.iterdir():
             if not p.is_file():
                 continue
+            name = p.stem
+            if "test_" in name:
+                continue
             ret.append(p.stem)
         return ret
 
     def run(self, script_name: str) -> None:
-        pass
+        fname = shortcuts_dir / f"{script_name}.script"
+        with open(fname, "rb") as f:
+            tokens = list(tokenize(f.readline))
+
+
+        index = 0
+
+        while index < len(tokens):
+            token = tokens[index]
+            if token.type in [ENCODING, ENDMARKER, NEWLINE]:
+                index+=1
+            elif token.type == NAME:
+                index += 1
+                op = tokens[index]
+                assert op.type == OP and op.string == "("
+
+                args = []
+                while True:
+                    index+=1
+                    t = tokens[index]
+                    if t.type == OP and t.string == ")":
+                        index+=1
+                        break
+                    elif t.type == OP and t.string == ",":
+                        continue
+                    elif t.type in [NAME, NUMBER]:
+                        args.append(t)
+
+                if token.string == "sleep":
+                    self._sleep(args)
+                else:
+                    raise NotImplementedError
+            else:
+                raise NotImplementedError
+
+
+    def _sleep(self, args: list[TokenInfo]) -> None:
+        assert len(args) == 1 
+        assert args[0].type == NUMBER
+        self._adb.sleep(int(args[0].string))
--- be/be/test_decode_script.py
+++ be/be/test_decode_script.py
@@ -1,8 +1,14 @@
 from .decode_script import DecodeScript
+from .adb_helper import AdbHelper
 
 
 def test_get_scripts() -> None:
-    scripts = DecodeScript()
+    scripts = DecodeScript(AdbHelper())
     result = scripts.scripts()
     assert "delete_account" in result
     assert "go_title_from_lobby" in result
+    assert "test_script" not in result
+
+def test_run() -> None:
+    scripts = DecodeScript(AdbHelper())
+    scripts.run("test_script")
